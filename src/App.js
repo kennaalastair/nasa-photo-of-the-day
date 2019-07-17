c3c6448dfa2e70aea185d03980880fd34bf043c1
Kenna <kennaalastair@gmail.com>
2019-07-17 16:35:29 -0700

    ApodGrid, got APOD data with axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import ApodGrid from "./Components/ApodGrid";
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun <span role='img' aria-label='rocket'>🚀</span>!
       </p>
+      <ApodGrid data />
     </div>
   );
 }
---
+++ src/Components/ApodCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ApodCard({ imgUrl }) {
+    return (
+        <div className="apod-card">
+            {/* <img src={imgUrl} style={{ maxWidth: "175px" }} /> */}
+        </div>
+    );
+}
